805dbaa61bfb1a1f1e3856e5c5b56d697e6a33b4
Halbard <HalbardHobby@users.noreply.github.com>
2022-07-09 17:22:58 -0500

    ✨ Added state rendering

--- state.go
+++ state.go
@@ -5,18 +5,17 @@ import (
 	"time"
 )
 
+const empty, alive rune = '\u3000', '😸'
+
 type State struct {
-	State_0, State_1        [nWorldHeight * nWorldWidth]bool
+	State_0, State_1        []bool
 	CurrentState, NextState []bool
 }
 
 func InitState() *State {
 	s := new(State)
-
-	for i := 0; i < nWorldHeight*nWorldWidth; i++ {
-		s.State_0[i] = false
-		s.State_1[i] = false
-	}
+	s.State_0 = make([]bool, nWorldHeight*nWorldWidth)
+	s.State_1 = make([]bool, nWorldHeight*nWorldWidth)
 
 	s.CurrentState = s.State_0[:]
 	s.NextState = s.State_1[:]
@@ -25,12 +24,12 @@ func InitState() *State {
 }
 
 func (s *State) GetTotalNeighbors(x, y int) (total int) {
-	for j := x - 1; j < x+1; j++ { // check neighbouring cells
-		for i := y - 1; i < y+1; i++ {
+	for j := x - 1; j <= x+1; j++ {
+		for i := y - 1; i <= y+1; i++ {
 			// Check if current coordinate is beyond border
-			if !(j < 0 || i < 0 || j >= nWorldWidth || i >= nWorldHeight) {
+			if !(j < 0 || j >= nWorldWidth || i < 0 || i >= nWorldHeight) {
 				// Check if not given coordinates
-				if s.CurrentState[j*nWorldWidth+i] && !(i == x && j == y) {
+				if s.CurrentState[(i*nWorldWidth)+j] && !(i == y && j == x) {
 					total += 1
 				}
 			}
@@ -43,10 +42,10 @@ func (s *State) IterateState() {
 	for j := 0; j < nWorldHeight; j++ {
 		for i := 0; i < nWorldWidth; i++ {
 			neighbors := s.GetTotalNeighbors(i, j)
-			if s.CurrentState[j*nWorldWidth+i] {
-				s.NextState[j*nWorldWidth+i] = neighbors >= 2 && neighbors <= 3
+			if s.CurrentState[(j*nWorldWidth)+i] {
+				s.NextState[(j*nWorldWidth)+i] = neighbors >= 2 && neighbors <= 3
 			} else {
-				s.NextState[j*nWorldWidth+i] = neighbors == 3
+				s.NextState[(j*nWorldWidth)+i] = neighbors == 3
 			}
 		}
 	}
@@ -57,6 +56,20 @@ func (s *State) IterateState() {
 func (s *State) SetRandomState() {
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < nWorldWidth*nWorldHeight; i++ {
-		s.CurrentState[i] = rand.Intn(3) == 1
+		s.CurrentState[i] = rand.Intn(5) == 1
 	}
+}
+
+func (s *State) Render() (out string) {
+	for j := 0; j < nWorldHeight; j++ {
+		for i := 0; i < nWorldWidth; i++ {
+			if s.CurrentState[(j*nWorldWidth)+i] {
+				out += string(alive)
+			} else {
+				out += string(empty)
+			}
+		}
+		out += "\n"
+	}
+	return
 }
